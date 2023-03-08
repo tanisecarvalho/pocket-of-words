8137061c21c8742a8c92fda23f8dc6913c3635d4
Tanise Carvalho <carvalhotanise@gmail.com>
2023-03-08 10:36:54 +0000

    feat: add function print_card

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -12,8 +13,41 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('pocket_of_words')
 
-first = SHEET.worksheet('tanise_eng_por')
+default_card_content = ["WORD", "Here a sentence to help you remember the word.", "ANSWER"]
 
-data = first.get_all_values()
+def print_card(card_content):
+    """
+    Prints the card for review
+    """
+    word_index = 0
+    CARD_SIZE = 58
 
-print(data)
+    card = ''
+    card_sides = ''
+
+    for i in range(CARD_SIZE):
+        card += '-'
+
+    for i in range(10):
+        card_sides += '|'
+        count_spaces = CARD_SIZE
+        if i in (1, 4, 8):
+            word = card_content[word_index]
+            word_index += 1
+            count_spaces = CARD_SIZE - len(word)
+            print(word, count_spaces)
+            for i in range(count_spaces):
+                if i == int(count_spaces / 2):
+                    card_sides += word
+                card_sides += ' '
+        elif i == 6:
+            card_sides += card
+        else:
+            for i in range(count_spaces):
+                card_sides += ' '
+        card_sides += '|\n'
+
+    card_final = ' ' + card + '\n' + card_sides + ' ' + card
+    print(card_final.center(80))
+
+print_card(default_card_content)
